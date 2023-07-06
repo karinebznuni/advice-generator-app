01292f4dc4fdd7c83c3b20d1088d2032e57ed380
karinebznuni <karine-13.01.94@bk.ru>
2023-07-06 12:45:42 +0400

    Add homework

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import divider from "./assets/divider.svg";
+import dice from "./assets/dice.svg";
+
+function App() {
+  const [adviceNumber, setAdviceNumber] = useState(null);
+  const [adviceText, setAdviceText] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getAdvice = async () => {
+    try {
+      setIsLoading(true);
+
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const data = await response.json();
+      const slip = data.slip;
+      const number = slip.id;
+      const text = slip.advice;
+
+      setAdviceNumber(number);
+      setAdviceText(text);
+    } catch (error) {
+    } finally {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1000);
+    }
+  };
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
+  return (
+    <>
+      <div className="container">
+        <div className="card">
+          <h1 className="title">ADVICE #{adviceNumber}</h1>
+          <span className="advice">{adviceText}</span>
+          <img src={divider} alt="" className="divider" />
+          <div className={`dice-icon ${isLoading && "rotate"}`}>
+            <img src={dice} alt="" onClick={getAdvice} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
