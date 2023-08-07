032a8435fbbfe96a8c22e5644483c93fbdd9b421
karinebznuni <104342716+karinebznuni@users.noreply.github.com>
2023-08-07 13:09:12 +0400

    Update src/App.jsx
    Co-authored-by: Tigran Karakhanyan <karakhanyant@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -38,9 +38,11 @@ function App() {
         <h1 className="title">ADVICE #{adviceNumber}</h1>
         <span className="advice">{adviceText}</span>
         <img src={divider} alt="" className="divider" />
-        <div className={`dice-icon ${isLoading && "rotate"}`}>
-          <img src={dice} alt="" onClick={getAdvice} />
-        </div>
+        <button
+          className={`dice-icon ${isLoading ? "rotate" : ""}`} onClick={getAdvice}
+        >
+          <img src={dice} alt="" />
+        </button>
       </div>
     </>
   );
